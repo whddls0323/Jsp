30773fb1f7cb03667b1df865a6f747d05e5a8056
whddls0323 <whddls0323@naver.com>
2025-08-01 17:48:56 +0900

    add ch09

---
+++ ch09/src/main/java/controller/user2/ListController.java
@@ -0,0 +1,8 @@
+package controller.user2;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/user2/list.do")
+public class ListController extends HttpServlet {
+}
